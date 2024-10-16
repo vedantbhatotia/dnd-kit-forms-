a5cf9bb78c7f38ced1dc0584381922b585024854
vedantbhatotia <vedantbhatotia@gmail.com>
2024-10-16 23:29:04 +0530

    shadcn install

--- my-app/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ my-app/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,15 +1,5 @@
-// import { SignIn } from '@clerk/nextjs'
 
-// export default function Page() {
-//   return <SignIn />
-// }
 import { SignIn } from "@clerk/nextjs";
-
-// export default function Page() {
-//   return <SignIn />;
-// }
-import { SignUp } from "@clerk/nextjs";
-
 export default function Page() {
   return(
     <>
